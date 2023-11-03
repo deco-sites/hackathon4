50a70bf7463cf59419181d81e189f5703f484e1a
Álvaro Martinez Ferreira <alvaromartinezferreira@gmail.com>
2023-11-02 23:42:54 -0300

    add colors

--- components/ui/ListColors.tsx
+++ components/ui/ListColors.tsx
@@ -20,23 +20,34 @@ function ListColors(props: Props) {
   const removeColor = function () {
     setNumberColors(numberColors > 2 ? numberColors - 1 : numberColors);
   };
+  const submit = function(event:any){
+    event.preventDefault();
+    let colors = [];
+    for(let i=0;i<numberColors;i++){
+      colors.push(event.target[`color-${i}`].value)
+    }
+    console.log(colors)
+  }
   return (
     <div className="h-full relative my-0 flex flex-col justify-between">
       <div>
         <button className="font-bold text-2xl bg-red-600 w-8 h-8 pb-1 rounded-lg justify-center text-center" onClick={() => removeColor()}>-</button>
         <button className="font-bold text-2xl bg-blue-600 w-8 h-8 pb-1 rounded-lg justify-center text-center" onClick={() => addColor()}>+</button>
       </div>
-      <ul className="relative flex h-full">
-        {[...Array(numberColors)].map((elementInArray, index) => (
-          <li className="flex-1 relative h-fit" key={index}>
-            <label
-              htmlFor={`color-${index}`}
-              className="absolute h-full w-full bg-transparent flex items-center shadow-[inset_0_-2px_4px_rgba(0,0,0,0.6)] rounded-lg"
-            ></label>
-            <PickerColor value="#ffffff" id={`color-${index}`} />
-          </li>
-        ))}
-      </ul>
+      <form onSubmit={submit}>
+        <ul className="relative flex h-full">
+          {[...Array(numberColors)].map((elementInArray, index) => (
+            <li className="flex-1 relative h-fit" key={index}>
+              <label
+                htmlFor={`color-${index}`}
+                className="absolute h-full w-full bg-transparent flex items-center shadow-[inset_0_-2px_4px_rgba(0,0,0,0.6)] rounded-t-lg"
+              ></label>
+              <PickerColor value="#ffffff" id={`color-${index}`} />
+            </li>
+          ))}
+        </ul>
+        <button type="submit">Publicar</button>
+      </form>
     </div>
   );
 }
